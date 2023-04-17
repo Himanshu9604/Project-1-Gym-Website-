8d0ee37c7928720bb1f7e706de39fa1b0e2a4721
Himanshu <himanshuyadav201000@gmail.com>
2023-04-17 19:39:03 +0530

    Registration page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/AboutUs";
 import Blogs from "./pages/Blogs";
-import Classes from "./pages/Classes";
+import Trainer from "./pages/Trainer";
 import JoinUs from "./pages/JoinUs";
 import Navbar from "./components/Navbar";
 import Pricing from "./pages/Pricing";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Registration/>} />
         <Route path="/about" element={<About />} />
         <Route path="/blogs" element={<Blogs />} />
-        <Route path="/classes" element={<Classes />} />
+        <Route path="/trainer" element={<Trainer />} />
         <Route path="/joinus" element={<JoinUs />} />
         <Route path="/pricing" element={<Pricing/>} />
+        <Route path="/registration" element={<Registration/>} />
       </Routes>
     </div>
   );
