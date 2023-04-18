5297defe6f381bab0258680a64d4fd4551916179
Chandra-Bhan-Prajapati <prajapatichandrabhan333@gmail.com>
2023-04-18 13:42:07 +0530

    Minners Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 // <<<<<<< HEAD
-// import styles from './Navbar.module.css'
-// function Navbar() {
-//   return (
-//     <div className={styles.header}>
-//       <NavLink to='/' className={styles.home}>Home</NavLink>
-//       <NavLink to='/about' className={styles.About}>  About</NavLink>
-//       <NavLink to='/blogs' className={styles.Blogs}>Blogs</NavLink>
-//       <NavLink to='/classes' className={styles.Classes}>Classes</NavLink>
-//       <NavLink to='/pricing' className={styles.Pricing} >Pricing</NavLink>
-//       <NavLink to='/joinus'  className={styles.JoinUs}> <button className={styles.button}>JoinUs</button></NavLink>
-
-// =======
-import Styles from "./Navbar.module.css";
-
+import styles from "./Navbar.module.css";
+function Navbar() {
+  return (
+    <div className={styles.header}>
+      <NavLink to="/" className={styles.home}>
+        Home
+      </NavLink>
+      <NavLink to="/about" className={styles.About}>
+        {" "}
+        About
+      </NavLink>
+      <NavLink to="/blogs" className={styles.Blogs}>
+        Blogs
+      </NavLink>
+      <NavLink to="/classes" className={styles.Classes}>
+        Classes
+      </NavLink>
+      <NavLink to="/pricing" className={styles.Pricing}>
+        Pricing
+      </NavLink>
+      <NavLink to="/joinus" className={styles.JoinUs}>
+        {" "}
+        <button className={styles.button}>JoinUs</button>
+      </NavLink>
+      {/* // ======= import Styles from "./Navbar.module.css"; */}
+      {/* 
 function Navbar() {
   return (
     <div className={Styles.navbar}>
@@ -40,7 +52,7 @@ function Navbar() {
         <li>
           <NavLink to="/login"> Login</NavLink>
         </li>
-      </ul>
+      </ul>*/}
       {/* >>>>>>> origin/login-pages */}
     </div>
   );
