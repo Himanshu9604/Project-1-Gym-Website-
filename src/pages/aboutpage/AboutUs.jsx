bbe70dd594dfea0d6eb7487d8e3c105c451d2126
ishaGupta23 <ishagupta77@gmail.com>
2023-04-19 15:50:03 +0530

    Update Pricing page, NAvbar page, AboutUS page, Training page and all module.css

--- src/pages/aboutpage/AboutUs.jsx
+++ src/pages/aboutpage/AboutUs.jsx
@@ -4,6 +4,27 @@ import Styles from "./AboutUs.module.css";
 function About() {
   return (
     <div>
+      <div>
+      <div>
+
+      <iframe  width="1510"
+         height="440" class="shogun-video-embed " src="https://www.youtube.com/embed/lpxKE4gLn8U?start=0&end=0&autoplay=1&loop=1&mute=1&playlist=lpxKE4gLn8U&version=3&rel=0" allowfullscreen title="Embedded Youtube Video">…</iframe>
+
+        
+         </div>
+      </div>
+      <div className={Styles.nopain}>
+        <iframe
+        
+          width="1510"
+          height="340"
+          src="https://www.youtube.com/embed/-Ov9JTw-59s"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>{" "}
+      </div>
       <div className={Styles.main}>
         <div className={Styles.first}>
           <div className={Styles.leftimg}>
@@ -17,9 +38,10 @@ function About() {
               allowfullscreen
             ></iframe>
           </div>
+
           <div className={Styles.rightcontent}>
-            <h1 style={{ color: "tomato", fontSize: "80px" }}>About Us</h1>
-            <pre style={{ fontSize: "30px", marginLeft: "60px" }}>
+            <h1 style={{ color: "tomato", fontSize: "80px" }}> 🏋🏻‍♂️About Us</h1>
+            <pre style={{ fontSize: "23px", marginLeft: "50px" }}>
               Find motivation from training with others for an efficient and
               <br />
               progressive workout. Offering interactive and energetic sessions,
@@ -36,8 +58,8 @@ function About() {
               style={{
                 backgroundColor: "red",
                 fontWeight: "bold",
-                fontSize: "30px",
-                borderRadius: "15px",
+                fontSize: "20px",
+                borderRadius: "10px",
               }}
             >
               Learn More
@@ -51,7 +73,7 @@ function About() {
 
         <div className={Styles.second}>
           <div className={Styles.leftcontent}>
-            <h1 style={{ color: "tomato", fontSize: "80px" }}>Why Choose US</h1>
+            <h1 style={{ color: "tomato" }}>Why Choose US</h1>
             <p>
               <li> 💪Consulation With expert</li>
             </p>
@@ -81,6 +103,39 @@ function About() {
             ></iframe>
           </div>
         </div>
+        <div className={Styles.culture}>
+          <h1>Our Culture</h1>
+          <p style={{ color: "black" }}>
+            We know that building a positive culture is incredibly important. We
+            believe in encouraging, supporting, challenging, learning and
+            growing to be the best! Our flexible working solutions, <br />
+            gym rebates and educational opportunities create a positive
+            work/life balance for all our employees.
+            <br />
+            <br />
+            We believe in giving back and offering time, money and products to
+            volunteer services and community organisations.
+            <br />
+            We’re committed to improving individuals' health, fitness, and
+            well-being by supporting global and local charities, local sports
+            teams, and events.
+            <br />
+            If you're looking for an opportunity to grow at a company that's
+            empowered, inclusive, and values great work—you should work with us.
+          </p>
+        </div>
+        <div>
+          <iframe
+            width="1490"
+            height="540"
+            src="https://www.youtube.com/embed/HUlmP6cPXvQ"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
+         
       </div>
     </div>
   );
